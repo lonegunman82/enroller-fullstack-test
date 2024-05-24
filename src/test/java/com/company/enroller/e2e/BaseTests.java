158cde80ee6b775ea0f5a714b51499952d27ff94
Łukasz <kordeczka@agh.edu.pl>
2024-05-24 14:47:05 +0200

    Added built frontend as resources

--- src/test/java/com/company/enroller/e2e/BaseTests.java
+++ src/test/java/com/company/enroller/e2e/BaseTests.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 public class BaseTests {
--- src/test/java/com/company/enroller/e2e/Const.java
+++ src/test/java/com/company/enroller/e2e/Const.java
@@ -2,7 +2,7 @@ package com.company.enroller.e2e;
 
 public class Const {
 
-    public final static String HOME_PAGE = "http://localhost:3000/";
+    public final static String HOME_PAGE = "http://localhost:8080/";
     public final static String EXIST_USER_1_MAIL = "user1@gmail.com";
     public final static String EXIST_USER_2_MAIL = "user2@gmail.com";
     public final static String NEW_USER_MAIL = "user3@gmail.com";
